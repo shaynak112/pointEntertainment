26fb7bd76cc641bba82a94354882260d1f449632
Shayna <shaynak112@gmail.com>
2017-05-28 12:37:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+﻿
+
+$('.aboutImage').click(function()
+	{
+
+	$('.DJinfo').hide();
+    $('.aboutImage').css("border-color", "#32CD32");
+    $(this).next('.DJinfo').show();
+    $(this).css("border-color", "#FF66CC");
+
+	}
+
+	);
+
+
+
