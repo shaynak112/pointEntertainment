f59085fcead947bbef503d701bf7e6d945a18956
Shayna <shaynak112@gmail.com>
2017-05-28 12:40:00 -0400

    Add files via upload

---
+++ previousEvents.php
@@ -0,0 +1,119 @@
+ <!-- Previous Events -->
+    <section id="previousEvents" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Previous Events</h2>
+                <p>Some of our previous events include "Old School Tribal", "Tech House Bombs", and "Classics VS Techno". We have also been lucky enough to book some amazing headliners such as Agent Orange, Nick Bertossi, Durty Soxxx, Daniel Dubb, Mladen Tomic, Flipside, and Zentrix. Here are the flyers from a few of our past events.</p>
+            </div>
+
+            <div class='row'>
+                <div class="col-lg-12">
+
+                    <div class="col-lg-4">
+                    <a href="https://www.facebook.com/events/728053134039088/" class="previousFlyers" target="_blank"><img src="previousEvents/event17.jpg" class="previousFlyers"></a>
+                    </div>
+
+                    <div class="col-lg-4">
+                    <a href="https://www.facebook.com/events/1412484415697537/" class="previousFlyers" target="_blank"><img src="previousEvents/event01.jpg" class="previousFlyers"></a>
+                    </div>
+
+                    <div class="col-lg-4">
+                    <a href="https://www.facebook.com/events/226948664376757/" class="previousFlyers" target="_blank"><img src="previousEvents/event13.jpg" class="previousFlyers"></a>
+                    </div>
+
+                </div>
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
+
+            <div class='row'>
+                <div class="col-lg-12">
+
+                    <div class="col-lg-4">
+                        <a href="https://www.facebook.com/events/1736636256548520/" class="previousFlyers" target="_blank"><img src="previousEvents/event10.jpg" class="previousFlyers"></a>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <a href="https://www.facebook.com/events/1029483050529087/" class="previousFlyers" target="_blank"><img src="previousEvents/event22.jpg" class="previousFlyers"></a>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <a href="https://www.facebook.com/events/995797440459242/" class="previousFlyers" target="_blank"><img src="previousEvents/event08.jpg" class="previousFlyers"></a>
+                    </div>
+
+                </div>
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
+
+            <div class='row'>
+                <div class="col-lg-12">
+
+                    <div class="col-lg-4">
+                        <a href="https://www.facebook.com/events/1700379413509584/" class="previousFlyers" target="_blank"><img src="previousEvents/event09.jpg" class="previousFlyers"></a>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <a href="https://www.facebook.com/events/820066108012040/" class="previousFlyers" target="_blank"><img src="previousEvents/event15.jpg" class="previousFlyers"></a>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <a href="https://www.facebook.com/events/228682417618979/" class="previousFlyers" target="_blank"><img src="previousEvents/event25.jpg" class="previousFlyers"></a>
+                    </div>
+
+                </div>
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
+
+            <div class='row'>
+                <div class="col-lg-12">
+
+                    <div class="col-lg-4">
+                        <a href="https://www.facebook.com/events/375978646120821/" class="previousFlyers" target="_blank"><img src="previousEvents/event16.jpg" class="previousFlyers"></a>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <a href="https://www.facebook.com/events/819773584854508/" class="previousFlyers" target="_blank"><img src="previousEvents/event21.jpg" class="previousFlyers"></a>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <a href="https://www.facebook.com/events/1495075123896353/" class="previousFlyers" target="_blank"><img src="previousEvents/event20.jpg" class="previousFlyers"></a>
+                    </div>
+
+                </div>
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
+
+            <div class='row'>
+                <div class="col-lg-12">
+
+                    <div class="col-lg-4">
+                        <a href="https://www.facebook.com/events/860034584072419/" class="previousFlyers" target="_blank"><img src="previousEvents/event07.jpg" class="previousFlyers"></a>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <a href="https://www.facebook.com/events/1459370677677423/" class="previousFlyers" target="_blank"><img src="previousEvents/event05.jpg" class="previousFlyers"></a>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <a href="https://www.facebook.com/events/226948664376757/" class="previousFlyers" target="_blank"><img src="previousEvents/event11.jpg" class="previousFlyers"></a>
+                    </div>
+
+                </div>
+            </div>
+
+          
+
+
+        </div>
+    </section>
