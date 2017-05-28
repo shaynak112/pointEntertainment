e46c58d999b1a2bd8dd8966eef851fed2f9beb31
Shayna <shaynak112@gmail.com>
2017-05-28 12:38:52 -0400

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,139 @@
+ <!-- Gallery -->
+    <section id="gallery" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Gallery</h2>
+                    <p>Point has hosted many events including various international DJs such as Durty Soxxx, Nick Bertossi, Mladen Tomic, Agent Orange, and more. And we have a few more coming up.</p>
+
+                    <p>Let's Dance has become a staple in the techno community for friendly events with local, quality DJs for the past few years and we are currently expanding so that we can bring in both international DJs as well as host local events.</p>
+
+        <p>Click on each image for a brief description.</p>
+
+    <div class="row">
+        <div class='col-lg-12'>
+            
+                <div class="col-lg-3">
+                    <img src="gallery/img_01.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Jayforce pumping up the crowd at Populus/Toika (Kotsy)</p>
+                </div>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_02.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Deko-ze and Ticky Ty, an unstoppable pair</p>
+                </div>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_03.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Lights in action at Li'ly Lounge</p>
+                </div>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_04.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Fun night out at Populus/Toika (Kotsy)</p>
+                </div>
+            </div>
+        </div>
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+
+        <div class='row'>
+       <div class='col-lg-12'>
+            
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_05.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Jayforce and Billey Barry (Kotsy)</p>
+                </div>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_06.jpg" class="galleryImg" />
+                    <p class="galleryInfo">El Duran B2B Random Doug</p>
+                </div>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_07.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Panda USB stick (Kotsy)</p>
+                </div>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_08.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Enjoying a good night out (Kotsy)</p>
+                </div>
+            </div>
+        </div>
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+
+        
+            <div class='row'>
+            <div class='col-lg-12'>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_09.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Thien B2B Billey Barry (Kotsy)</p>
+                </div>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_10.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Enjoying the music and friends!</p>
+                </div>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_11.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Ticky Ty spinning!</p>
+                </div>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_12.jpg" class="galleryImg" id="tallPicLights" />
+                    <p class="galleryInfo">Always having fun! (Kotsy)</p>
+                </div>
+            </div>
+        </div>
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+
+        
+            <div class='row'>
+            <div class='col-lg-12'>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_13.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Crowd dancing along at Populus (Kotsy)</p>
+                </div>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_14.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Club 120 group for Old School Vinyl! Beginning til end</p>
+                </div>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_15.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Thien opening up at Li'ly Lounge</p>
+                </div>
+
+                <div class="col-lg-3">
+                    <img src="gallery/img_16.jpg" class="galleryImg" />
+                    <p class="galleryInfo">Deko-ze always on fire</p>
+                </div>
+            </div>
+        </div>
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+
+
+            </div>
+        </div>
+        
+    </section>
