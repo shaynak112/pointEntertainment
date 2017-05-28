d6ce6a0f6bbe856003d2a71eeb09f76ec58da34c
Shayna <shaynak112@gmail.com>
2017-05-28 12:41:55 -0400

    Add files via upload

---
+++ venues.php
@@ -0,0 +1,29 @@
+  <!-- Venues -->
+    <section id="venues" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Venues</h2>
+                <div id="venuesWrapper">
+
+<p>Point Entertainment has put on events at various clubs/lounges around Toronto including <a href="https://www.facebook.com/SoundXchange/" target="_blank" class="venueClass">Sound Xchange</a>, <a href="http://www.club120.ca/" target="_blank" class="venueClass">Club 120</a>, and <a href="https://www.facebook.com/populusclub/" target="_blank" class="venueClass">Populus</a> (previously Toika). We have worked with other DJs, promoters, and event planners for events at venues such as Coda, VintaGe, Comfort Zone, One Loft, Bassline, Nest, and Li'Ly Lounge.</p>
+
+
+
+<h2 class="venueH2">Sound Xchange</h2>
+
+<p>Sound Xchange is located at 421 Dundas Street West. To enter, you go through the mall and up the stairs to the left. Follow the beats. They are open Thursdays, Fridays, Saturdays, and Sundays for various events. Check out Sound Xchange's <a href="https://www.facebook.com/SoundXchange/" target="_blank">Facebook page</a> for more information.</p>
+
+<a href="https://www.facebook.com/SoundXchange/" target="_blank"><img src="venues/sxdecor.jpg" style='width:50%;display:inline-block;margin-left:0px;' title="Sound Xchange is open Thursdays, Fridays, Saturdays, Sundays."></a>
+
+<br/>
+<br/>
+
+<h2 class="venueH2">Club 120</h2>
+
+<p>Club 120 is located at 120 Church Street. The second level is the area where parties/events are held and the first level is a diner. They host a variety events - techno, afterhours, comedy. It is a prime venue for the LBGT community. There are two parking lots close by. Check out Club 120's <a href="https://www.facebook.com/Club-120-422254877790065/" target="_blank">Facebook page</a> for additional information.</p>
+
+
+</div>
+            </div>
+        </div>
+    </section>
