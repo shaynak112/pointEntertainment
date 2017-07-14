552ceffb587a49a205de4fc0e32f298433e88d98
Shayna <shaynak112@gmail.com>
2017-07-13 22:04:19 -0400

    Create script.js

---
+++ djs/script.js
@@ -0,0 +1,14 @@
+$('.aboutImage').click(function()
+	{
+
+	$('.DJinfo').hide();
+    $('.aboutImage').css("border-color", "#32CD32");
+    $(this).next('.DJinfo').show();
+    $(this).css("border-color", "#FF66CC");
+
+	}
+
+	);
+
+
+
