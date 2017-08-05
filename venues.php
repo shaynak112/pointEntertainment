be32cdadea43ae8c5e700ba0a7d8820d85907dd5
Shayna <shaynak112@gmail.com>
2017-08-05 08:32:24 -0400

    Add files via upload
    small updated with venues; nothing major, just requested by an assistant

--- venues.php
+++ venues.php
@@ -7,9 +7,9 @@
 
 <p>Point Entertainment has put on events at various clubs/lounges around Toronto including <a href="https://www.facebook.com/SoundXchange/" target="_blank" class="venueClass">Sound Xchange</a>, <a href="http://www.club120.ca/" target="_blank" class="venueClass">Club 120</a>, and <a href="https://www.facebook.com/populusclub/" target="_blank" class="venueClass">Populus</a> (previously Toika). We have worked with other DJs, promoters, and event planners for events at venues such as Coda, VintaGe, Comfort Zone, One Loft, Bassline, Nest, and Li'Ly Lounge.</p>
 
-<h2>Sound Xchange</h2>
+<!--<h2>Sound Xchange</h2>
 <p>Sound Xchange is located at 421 Dundas Street West. To enter, you go through the mall and up the stairs to the left. Follow the beats. They are open Thursdays, Fridays, Saturdays, and Sundays for various events. Check out Sound Xchange's <a href='https://www.facebook.com/SoundXchange/' target='_blank'></p>
-<p><a href='https://www.facebook.com/SoundXchange/' target='_blank'><img src='venues/sxdecor.jpg' style='width:50%;display:inline-block;margin-left:0px;' title='Sound Xchange'></a></p>
+<p><a href='https://www.facebook.com/SoundXchange/' target='_blank'><img src='venues/sxdecor.jpg' style='width:50%;display:inline-block;margin-left:0px;' title='Sound Xchange'></a></p>-->
 
 
 <?php
